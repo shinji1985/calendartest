c3cdd2590373734144ac53e672c7759ab1787547
shinji1985 <teamhack2007@gmail.com>
2018-05-03 09:21:04 +0700

    少し修正

--- src/pages/calendar/calendar.module.ts
+++ src/pages/calendar/calendar.module.ts
@@ -2,12 +2,22 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { CalendarPage } from './calendar';
 
+import { CalendarModule, CalendarDateFormatter, CalendarEventTitleFormatter } from 'angular-calendar';
+
+import { CalendarWeekHoursViewModule } from 'angular-calendar-week-hours-view';
+
+import { CustomEventTitleFormatterProvider } from '../../providers/custom-event-title-formatter/custom-event-title-formatter';
+import { CustomDateFormatterProvider } from '../../providers/custom-date-formatter/custom-date-formatter';
+
 @NgModule({
   declarations: [
     CalendarPage,
   ],
   imports: [
     IonicPageModule.forChild(CalendarPage),
+    CalendarModule,
+    CalendarDateFormatter,
+    CalendarEventTitleFormatter
   ],
 })
-export class CalendarPageModule {}
+export class CalendarPageModule { }
